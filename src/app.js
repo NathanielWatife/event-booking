60b4992e1a19c3b1a46583844a26c03d164a6078
NathanielWatife <awosanminathaniel0@gmail.com>
2025-02-11 23:52:19 +0100

    feat- setup server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const eventRoutes = require('./routes/eventRoutes');
+const { moduleExpression } = require('@babel/types');
+
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/api', eventRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+module.exports = app;
